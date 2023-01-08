71fbddb1e32a6df817eac21560f50f0e332a1e84
EJDiamond <emilydiamond321@gmail.com>
2023-01-08 12:21:23 +0000

    Add responsiveness to popular profile component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -13,6 +13,10 @@ import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
@@ -26,11 +30,10 @@ function PostPage() {
       try {
         const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
-          axiosReq.get(`/comments/?post=${id}`)
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        setComments(comments)
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -44,32 +47,39 @@ function PostPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>{currentUser ? (
-        <CommentCreateForm
-        profile_id={currentUser.profile_id}
-        profileImage={profile_image}
-        post={id}
-        setPost={setPost}
-        setComments={setComments}
-      />
-) : comments.results.length ? (
-  "Comments"
-) : null}
-{comments.results.length ? (
-  comments.results.map((comment) => (
-    <Comment
-      key={comment.id}
-      {...comment}
-      setPost={setPost}
-      setComments={setComments}
-    />
-  ))
-) : currentUser ? (
-  <span>No comments yet, be the first to comment!</span>
-) : (
-  <span>No comments... yet</span>
-)}
-</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
